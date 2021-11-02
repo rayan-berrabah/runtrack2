211521a8637e3da1bb412b465389f71efaf1d5bd
Rayan <91850540+rayan-berrabah@users.noreply.github.com>
2021-11-02 10:57:54 +0100

    commit

---
+++ jour01/job00/index.php
@@ -0,0 +1,10 @@
+<html>
+ 	<head>
+  		<title>Job 01</title>
+ 	</head>
+
+ 	<body>
+ 			<?php echo '<h1>Hello LaPlateforme!</h1>'; ?>
+ 	</body>
+
+</html>
---
+++ jour01/job01/index.php
@@ -0,0 +1,5 @@
+<?php
+
+string $ecole = 'LaPlateforme';
+
+?>
