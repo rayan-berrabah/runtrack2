db5310a5cb48726a54e0429e381ca9927299232d
Rayan <91850540+rayan-berrabah@users.noreply.github.com>
2021-11-02 11:28:24 +0100

    commit2

--- jour01/job01/index.php
+++ jour01/job01/index.php
@@ -1,5 +1,13 @@
 <?php
 
-string $ecole = 'LaPlateforme';
+    echo $str = 'LaPlateforme'; 
+    echo $str2 = "Vive";
+    echo $str3 = "Vive LaPlateforme !"; 
+
+        echo $int = "4";
+        echo $int = "10";
+
+        echo $bool = "true"; 
+        echo $bool = "false";
 
 ?>
---
+++ jour01/job02/index.php
@@ -0,0 +1,15 @@
+<html>
+ 	<head>
+  		<title>Job 02</title>
+ 	</head>
+
+ 	<body>
+		 					
+					<!-- TEXTE HELLO LAPLATEFORME -->
+ 			<?php echo '<h1>Hello LaPlateforme !</h1>'; ?> 
+			 
+			 		<!-- 2 EME TEXTE HELLO LA PLATEFORME -->
+			<?php echo '<h1>Hello LaPlateforme !</h1>'; ?> 
+ 	</body>
+
+</html>
