3f2e8f8791fdd812938103b1040c568170298d84
Rayan <91850540+rayan-berrabah@users.noreply.github.com>
2021-11-10 10:56:14 +0100

    Jour 4

---
+++ jour04/job01/index.php
@@ -0,0 +1,14 @@
+<?php
+
+var_dump($_GET);
+
+echo count($_GET);
+
+?>
+
+<form method="get">
+<input type="text" name="user">
+<input type="password" name="password">
+<input type="submit" name="submit">
+
+</form>
---
+++ jour04/job03/index.php
@@ -0,0 +1,47 @@
+<?php
+
+$str = "";
+
+foreach($_POST as $argument => $valeur ){
+    $str = $str. "<tr>
+            <td> $argument </td>
+            <td> $valeur </td>
+        <tr>";
+}
+
+?>
+
+<!DOCTYPE html>
+<html>
+    
+    <form action="./index.php" method="post">
+
+            <label for="first-name">nom </label>
+            <input type="text" id="nom" name="first-name">
+            <label for="last-name">prénom </label>
+            <input type="text" id="prénom" name="last-name">
+            <label for="mail">email</label>
+        	<input type="email" id="mail" name="mail">
+	        <label for="age">âge</label>
+	        <input type="number" id="age" name="age">
+            <input type="submit" value="envoyer">
+    </form>
+
+<table>
+        <thead>
+
+            <tr>
+                <th>Argument</th>
+                <th>Valeur</th>
+            </tr>
+         </thead>
+
+    <tbody>
+        
+<?php 
+
+            echo $str;  ?>
+        
+    </tbody>
+
+</table>
